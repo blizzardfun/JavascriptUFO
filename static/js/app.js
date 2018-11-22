48be4054721ac6b2a70e52eaa072f74b588e99f3
blizzardfun <38262823+blizzardfun@users.noreply.github.com>
2018-11-22 11:35:27 -0600

    comments added

--- static/js/app.js
+++ static/js/app.js
@@ -9,45 +9,36 @@ submitDate.on("click", function() {
   
     // Select the date input element and get the value
     var inputDate=d3.select("#datetime").property("value");
-console.log("date:",inputDate);
     //filter by date
     if (inputDate===""){
         var dateTable=tableData;
     } else {
-console.log("filtering date");
         var dateTable=tableData.filter(entry => entry.datetime === inputDate);   
     }
         // Select the city input element,get the value  and convert to lower case--------------------------------------
     var inputCity=d3.select("#city").property("value").toLowerCase();
-console.log("city:",inputCity); 
     //filter by city
     if (inputCity ===""){
         var cityTable=dateTable;
     } else {
-console.log("filtering city");
         var cityTable=dateTable.filter(entry=> entry.city === inputCity); 
     }
     // Select the state input element, get the value  and convert to lower case------------------------------------------
     var inputState=d3.select("#state").property("value").toLowerCase();
-console.log("state:",inputState); 
     //filter by state
     if (inputState ===""){
         var stateTable=cityTable;
     } else {
-console.log("filtering state");
         var stateTable = cityTable.filter(entry=> entry.state === inputState); 
     }
     // Select the shape input element, get the value and convert to lower case-------------------------
     var inputShape=d3.select("#shape").property("value").toLowerCase();
-console.log("shape:",inputShape); 
     //filter by shape
     if (inputShape ===""){
         var shapeTable=stateTable;
     } else {
-console.log("filtering shape");
         var shapeTable=stateTable.filter(entry=> entry.shape===inputShape); 
     }
-  
     // get result table and clear it
     var resultTable=d3.select("#ufo-table")
     d3.selectAll(".result-row").text("");
@@ -57,7 +48,7 @@ console.log("filtering shape");
         var row=resultTable.append("tr").attr("class","result-row");
         Object.entries(entry).forEach(function([key,value]) {
             var cell = row.append("td");
-
+                 // print states and country all caps - capitalize city
             if (key === "state" || key === "country"){
                 cell.text(value).style("text-transform" ,"uppercase");    
             } else if (key == "city") {
